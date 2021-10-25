4b2bd726d2396fe143f3581c905c98e9680a05a4
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-25 20:47:26 +0900

    chore: 사용한 Hibernate 메소드 명시

--- src/main/java/com/programmers/heavenpay/store/repository/StoreRepository.java
+++ src/main/java/com/programmers/heavenpay/store/repository/StoreRepository.java
@@ -6,5 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    @Override
+    Optional<Store> findById(Long aLong);
+
+    @Override
+    void delete(Store entity);
+
+    @Override
+    <S extends Store> S save(S entity);
+
     Optional<Store> findByName(String name);
 }
