ad12c5ce716da6e3ae2ed28a1873b05b6487fea7
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-26 13:28:25 +0900

    fix: getter binding

--- src/main/java/com/programmers/heavenpay/store/dto/response/StoreInfoResponse.java
+++ src/main/java/com/programmers/heavenpay/store/dto/response/StoreInfoResponse.java
@@ -2,10 +2,12 @@ package com.programmers.heavenpay.store.dto.response;
 
 import com.programmers.heavenpay.store.entity.vo.StoreType;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Getter
 public class StoreInfoResponse {
     private final Long id;
 
