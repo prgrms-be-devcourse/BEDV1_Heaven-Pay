947c0b7b77316beee9426cb4247ed6ac119db9c4
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-26 16:34:32 +0900

    fix: Request 객체 builder  추가

--- src/main/java/com/programmers/heavenpay/store/dto/request/StoreCreateRequest.java
+++ src/main/java/com/programmers/heavenpay/store/dto/request/StoreCreateRequest.java
@@ -1,11 +1,13 @@
 package com.programmers.heavenpay.store.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Getter
+@Builder
 public class StoreCreateRequest {
     @Pattern(regexp = "\\S{2,10}", message = "store name은 공백없는 2~10자이어야 합니다")
     private String name;
