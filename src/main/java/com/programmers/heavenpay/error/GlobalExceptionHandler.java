27dd0b9334d92c151a7a905fd6de8f2726ea89e1
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-26 17:37:04 +0900

    feat: Dto 예외처리 추가

--- src/main/java/com/programmers/heavenpay/error/GlobalExceptionHandler.java
+++ src/main/java/com/programmers/heavenpay/error/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.programmers.heavenpay.error;
 
 import com.programmers.heavenpay.error.exception.NotDefinitionException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(NotDefinitionException.class)
     protected ResponseEntity<ErrorResponseDto> handleNotDefinitionException(NotDefinitionException exception) {
+        return makeErrorResponse(exception);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> methodValidException(MethodArgumentNotValidException exception){
         return makeErrorResponse(exception);
     }
 
